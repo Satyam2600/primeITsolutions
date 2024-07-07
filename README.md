4e320b6c15db79f84e3a694cf3d68c77d7dc713f
Ayush Chugh <hey@ayushchugh.com>
2024-07-07 15:20:03 +0530

    chore: :rocket: initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Setup locally
+
+1. Clone the repository
+1. Run `yarn install`
+1. Create a `.env` file in the root of the project and add environment variables mentioned below
+1. Run server by using: `yarn dev`
+
+### Environment Variables
+
+You can also take reference from `.env.example` file
+
+1. `PORT` - Port on which server will run
+1. `MONGO_URI` - MongoDB connection string
+
+### Important points
+
+1. We are using `ejs` as our template engine so you can't use `.html` you have to use `.ejs` extension
+1. All the views are in `views` folder
+1. All static files such as css, js, images are in `public` folder
---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+config();
+
+const app = express();
+const DB_URI =
+	process.env.MONGO_URI || "mongodb://localhost:27017/primeItSolutions";
+const PORT = process.env.PORT || 8000;
+
+// basic configuration
+app.set("view engine", "ejs");
+const viewsPath = path.join(__dirname, "views");
+app.set("views", viewsPath);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+const startServer = async () => {
+	// connect to database
+	await mongoose.connect(DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	console.log("Connected to database");
+
+	// start server
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
+};
+
+startServer();
